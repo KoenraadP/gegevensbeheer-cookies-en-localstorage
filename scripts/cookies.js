caaa1a0b1a82213cf3f54acfeb010f3c921ac133
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-02-20 09:29:11 +0100

    Version 9:29

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,1 +1,22 @@
 "use strict";
+
+// juiste gebruikersnaam en wachtwoord instellen
+const correctUser = "creo";
+const correctPassword = "creo";
+
+// HTML input tags koppelen
+const userInput = document.getElementById("user-name");
+const passwordInput = document.getElementById("password");
+
+// HTML form koppelen
+const form = document.getElementById("form");
+
+// submit event koppelen aan form
+form.addEventListener("submit", function(){
+    // controleren of login gegevens correct zijn
+    if (userInput.value === correctUser
+        && passwordInput.value === correctPassword) {
+            // doorgaan naar pagina success.html
+            window.open("success.html");
+        }    
+})
