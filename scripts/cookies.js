1d050b56f05b12cf3b36df88d13c63cc6e81a74a
koenraad.pecceu <koenraad.pecceu@gmail.com>
2025-02-20 10:28:06 +0100

    Version 10:28

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -11,12 +11,64 @@ const passwordInput = document.getElementById("password");
 // HTML form koppelen
 const form = document.getElementById("form");
 
+// extra cookie for testing purposes
+document.cookie = "school=Creo";
+
+// huidige cookies tonen in console
+console.log(document.cookie);
+
+// controleren of er een cookie loggedIn is met value "true"
+if (GetCookie("loggedIn") === "true") {
+    // als dit zo is --> automatisch naar volgende pagina
+    location.href = "success.html";
+}
+
 // submit event koppelen aan form
-form.addEventListener("submit", function(){
+form.addEventListener("submit", function(e){
+    // standaard gedrag formulier tegenhouden
+    e.preventDefault();
     // controleren of login gegevens correct zijn
     if (userInput.value === correctUser
         && passwordInput.value === correctPassword) {
+            // opslaan via cookies dat de login gelukt is
+            // een cookie bestaat altijd uit een key en een value
+            // bijvoorbeeld: user=Koenraad
+            document.cookie = "loggedIn=true";
             // doorgaan naar pagina success.html
-            window.open("success.html");
+            // alternatieven: location.assign(), location.href =, ...
+            // window.open("success.html");
+            location.href="success.html";
         }    
 })
+
+// function die de waarde van een specifieke
+// cookie opzoekt en als return geeft
+// als mijn document.cookie bijvoorbeeld
+// "loggedIn=false; school=Creo" is
+// en ik doe GetCookie("school") dan
+// wil ik als return "Creo"
+function GetCookie(cookieName) {
+    let cookieValue = "";
+    // cookies apart in een array plaatsen
+    const cookiesArray = document.cookie.split("; ")
+    console.log(cookiesArray);
+    // loopen door cookiesArray
+    cookiesArray.forEach(element => {
+        // iedere cookie splitsen in key en value
+        const splitCookie = element.split("=");
+        // element 0 is de key
+        // element 1 is de value
+        console.log(splitCookie);
+        // controleren of de key van de huidige cookie 
+        // in de loop gelijk is aan de parameter cookieName
+        if (splitCookie[0] === cookieName){
+            // als dit gelijk is, de value als return waarde
+            cookieValue = splitCookie[1];
+        }
+    });
+    // op het einde ofwel de gevonden waarde als return
+    // ofwel een lege string
+    return cookieValue;
+}
+
+console.log(GetCookie("school"));
--- scripts/success.js
+++ scripts/success.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// afmelden knop koppelen
+const btnLogoff = document.getElementById("logoff");
+
+// click event voor knop
+btnLogoff.addEventListener("click",function(){
+    // als er geklikt wordt op Afmelden
+    // dan moet de loggedIn cookie verzet worden
+    // naar false en moet ik weer terug naar
+    // de login.html pagina
+    document.cookie = "loggedIn=false";
+    location.href = "login.html";
+})
